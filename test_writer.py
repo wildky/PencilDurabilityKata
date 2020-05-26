8895d1cc0c113842fae659a583cccd30cb0ef2c6
kyle.wildermuth <ND64857@DOW.COM>
2020-05-26 18:31:32 -0400

    add test for reducing pencil point durability for upper case

--- test_writer.py
+++ test_writer.py
@@ -33,6 +33,13 @@ class TestPencil(unittest.TestCase):
         expected_point_durability = initial_point_durability - len(new_text)
         self.assertEqual(pencil.point_durability, expected_point_durability)
    
+    def test_writing_upper_case_characters_reduces_point_durability_by_two(self):
+        initial_point_durability = 100
+        pencil = Pencil(initial_point_durability)
+        new_text = "BIRD"
+        pencil.write(new_text)
+        expected_point_durability = initial_point_durability - len(new_text)*2
+        self.assertEqual(pencil.point_durability, expected_point_durability)
 
 if __name__ == "__main__":
     unittest.main()
--- writer/pencil.py
+++ writer/pencil.py
@@ -3,4 +3,11 @@ class Pencil:
         self.point_durability = point_durability
 
     def write(self, new_text):
-        self.point_durability -= len(new_text)
+        for character in new_text:
+            self._write_character(character)
+
+    def _write_character(self, character):
+        if character.isupper():
+            self.point_durability -= 2
+        elif character.islower():
+            self.point_durability -= 1        
