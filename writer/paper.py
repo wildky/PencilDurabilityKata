5101965a4ac87057c71dcd8255765e4d83f1c71d
kyle.wildermuth <ND64857@DOW.COM>
2020-05-27 13:04:40 -0400

    add docstrings to Paper class

--- writer/paper.py
+++ writer/paper.py
@@ -1,6 +1,23 @@
 class Paper:
+    """
+    A material that can be written on.
+
+    Attributes
+    ----------
+    text : str
+        The text that is written on the paper.
+    """
+
     def __init__(self):
         self.text = ""
 
     def write(self, new_text):
+        """
+        Appends text to end of text existing on the paper.
+
+        Parameters
+        ----------
+        new_text : str
+            The new text that is to be added.
+        """
         self.text += new_text
