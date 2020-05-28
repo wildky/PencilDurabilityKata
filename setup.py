7d9925f67fcdcd4da307202eae38f11998812752
kyle.wildermuth <ND64857@DOW.COM>
2020-05-28 18:21:10 -0400

    add setup and license

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='writer',
+    version='0.1',
+    packages=['writer',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
